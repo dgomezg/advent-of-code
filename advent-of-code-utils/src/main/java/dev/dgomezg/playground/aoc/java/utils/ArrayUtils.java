95d9e338484ccdd972872abb45f3b43c64e5e90b
David Gómez G <dgomezg@gmail.com>
2024-12-02 10:03:47 +0100

    Added ArrayUtils to advent-of-code-utils

---
+++ advent-of-code-utils/src/main/java/dev/dgomezg/playground/aoc/java/utils/ArrayUtils.java
@@ -0,0 +1,21 @@
+package dev.dgomezg.playground.aoc.java.utils;
+
+import java.util.function.IntBinaryOperator;
+
+public class ArrayUtils {
+
+    public static int[] removeElementAt(int[] array, int index) {
+        int[] newArray = new int[array.length - 1];
+        System.arraycopy(array, 0, newArray, 0, index);
+        System.arraycopy(array, index + 1, newArray, index, array.length - index - 1);
+        return newArray;
+    }
+
+    public static int[] prepend(int value, int[] array) {
+        int[] newArray = new int[array.length + 1];
+        newArray[0] = value;
+        System.arraycopy(array, 0, newArray, 1, array.length);
+        return newArray;
+    }
+
+}
